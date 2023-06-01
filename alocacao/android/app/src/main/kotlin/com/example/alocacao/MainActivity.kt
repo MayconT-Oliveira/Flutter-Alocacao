b670f4cad21e2838542b2955b98ccaa184152d54
MayconT-Oliveira <maycon.oliveira150300@gmail.com>
2023-06-01 19:56:20 -0300

    V1

---
+++ alocacao/android/app/src/main/kotlin/com/example/alocacao/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.alocacao
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ alocacao/lib/views/alocacao.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Alocacao extends StatefulWidget {
+  const Alocacao({super.key});
+
+  @override
+  State<Alocacao> createState() => _AlocacaoState();
+}
+
+class _AlocacaoState extends State<Alocacao> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
